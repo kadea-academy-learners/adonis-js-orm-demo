ca0a5212ccbf0644e4ead21af198d25d8eb124b8
Randy-RM <randymuhema@gmail.com>
2025-03-11 16:40:56 +0100

    feat : add showAllUsers controller function

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,3 +1,9 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
 
-export default class UsersController {}
+export default class UsersController {
+  public async showAllUsers({ view }: HttpContext) {
+    const users = await User.all()
+    return view.render('pages/show_all_user', { users })
+  }
+}
