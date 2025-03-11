022a30fda18b96541d7f0ba0c6164a99b57a55d4
Randy-RM <randymuhema@gmail.com>
2025-03-11 16:54:32 +0100

    feat : add user controller routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,9 +9,15 @@
 
 import router from '@adonisjs/core/services/router'
 const FrontOfficesController = () => import('#controllers/front_offices_controller')
+const UsersController = () => import('#controllers/users_controller')
 
 router.get('/', [FrontOfficesController, 'homePage']).as('home')
 router.get('/about', [FrontOfficesController, 'aboutPage']).as('about')
 router.get('/contact', [FrontOfficesController, 'contactPage']).as('contact')
 router.get('/services', [FrontOfficesController, 'servicesPage']).as('services')
 router.get('/blog', [FrontOfficesController, 'blogPage']).as('blog')
+
+router.get('/user/show-all', [UsersController, 'showAllUsers']).as('showAllUsers')
+router.get('/user/create', [UsersController, 'createUser']).as('createUser')
+router.post('/user/store', [UsersController, 'storeUser']).as('storeUser')
+router.get('/user/:id/delete', [UsersController, 'deleteUser']).as('deleteUser')
